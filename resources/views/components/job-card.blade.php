0ea9e595580fe7a8dd2cf0ab841581c06690e186
bernard chisumo <bchisumo74@yahoo.com>
2024-05-02 20:18:30 +0100

    Tag component added

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -9,10 +9,9 @@
     <!-- List of Tags --->
     <div class="flex  justify-between items-center mt-auto">
         <div>
-            <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-            <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-            <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
         </div>
 
         <img src="http://placehold.it/50/50" alt="employer-logo"  class="rounded-xl">
