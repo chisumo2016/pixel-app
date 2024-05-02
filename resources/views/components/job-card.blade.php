4d921a7aaeb79ded8823c5b654f0fb8a0f804d11
bernard chisumo <bchisumo74@yahoo.com>
2024-05-02 19:51:13 +0100

    job card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8 font-bold">
+        <h2>Video Producer</h2>
+        <p>Full Time from $60, 000 </p>
+    </div>
+
+    <!-- List of Tags --->
+    <div class="flex  justify-between items-center mt-auto">
+        <div>
+            <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+            <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+            <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+
+        </div>
+
+        <img src="http://placehold.it/50/50" alt="employer-logo"  class="rounded-xl">
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    Tanzania
+    <!--Job Cards--->
+   <section>
+       <h3 class="mb-6">Top Jobs</h3>
+
+        <div class="grid lg:grid-cols-3 gap-8">
+            <x-job-card/>
+            <x-job-card/>
+            <x-job-card/>
+        </div>
+   </section>
+
 </x-layout>
