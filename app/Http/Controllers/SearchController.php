78f91855484ef64fd5e5e493fc96dc4042b28723
bernard chisumo <bchisumo74@yahoo.com>
2024-05-13 21:21:46 +0100

    Search Functionality Added

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+
+class SearchController extends Controller
+{
+    public function __invoke()
+    {
+        //dd(request('q'));
+        $jobs = Job::where('title', 'LIKE', '%'.request('q').'%')->get();
+
+        return view('search', compact('jobs'));
+
+    }
+}
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -7,6 +7,9 @@
             {{--                <input type="text" placeholder="PHP Developer ..."--}}
             {{--                       class="rounded-xl bg-white/5  border-white/10 px-5 py-4 w-full max-w-xl">--}}
             {{--            </form>--}}
+            <x-forms.form action="/search" class="mt-6">
+                <x-forms.input :label="false" name="q" placeholder="Web Developer"/>
+            </x-forms.form>
 
         </section>
 
@@ -32,11 +35,11 @@
 
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
+
             <div class="mt-6 space-y-6">
                 @foreach($jobs as $job)
                     <x-job-card-wide :$job/>
                 @endforeach
-
             </div>
         </section>
     </div>
